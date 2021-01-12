f659ff477e29b2e9cc2814802f65a52a8c922279
Alfred <akoo1@cougarmail.collin.edu>
2021-01-12 16:47:28 -0600

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import Layout from './components/Layout/Layout';
+import Layout from './container/Layout/Layout';
 import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder';
 
 function App() {
